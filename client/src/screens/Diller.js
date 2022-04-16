ccf4136e63094da44be3ebd77b0abea87426d42f
bogdan <bogdanbogdanovich109@gmail.com>
2022-04-16 13:13:34 +0300

    add diller screen and some styles

---
+++ client/src/screens/Diller.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Diller = () => {
+
+    return(
+        <div className="container">
+            <h2>Профиль диллера</h2>
+            <div className="diller__body">
+        
+            </div>
+            <div className="list_of_cars">
+
+            </div>
+        </div>
+    )
+
+}
+
+export default Diller
