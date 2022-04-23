cc11d744a8fea70156408525f62b61eb09d5267f
failjak <nikita.gushtyn200370@gmail.com>
2022-04-23 17:53:40 +0300

    made dealer filters

--- apps/api/dealer/views.py
+++ apps/api/dealer/views.py
@@ -2,11 +2,12 @@ from rest_framework.viewsets import ModelViewSet
 
 from apps.models.dealership import Dealer, DealerAddress
 from .serializers import DealerSerializer, DealerListSerializer, DealerAddressSerializer
+from .filters import DealerFilter
 
 
 class DealerViewSet(ModelViewSet):
     queryset = Dealer.objects.all()
-    # serializer_class = DealerSerializer
+    filterset_class = DealerFilter
 
     def get_serializer_class(self):
         if self.action in ('list', 'retrieve'):
